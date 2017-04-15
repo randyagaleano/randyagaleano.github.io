15fadaede0a5ed4d72cf050835fd37a2c3484f64
randyagaleano <randyagaleano@gmail.com>
2017-04-15 17:42:01 -0400

    expressless again

--- routes/portfolio.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// get portfolio
-router.get('/', function(req, res, next) {
-	res.render('portfolio', { title: 'Express'} );
-});
-
-module.exports = router;
