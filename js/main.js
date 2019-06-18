0ab4b3c33bb1b17a361a73f813580fcfb27a62d9
Randy Galeano <randyagaleano@gmail.com>
2019-06-18 19:33:31 -0400

    new layout working, will clean up unused files once layout set

--- js/main.js
+++ js/main.js
@@ -1,55 +1,74 @@
-$('#menu-toggle').click(function(click) {
-    click.preventDefault();
-    $('#wrapper').toggleClass('toggled');
-});
+// $('#menu-toggle').click(function(click) {
+//     click.preventDefault();
+//     $('#wrapper').toggleClass('toggled');
+// });
 
-$('#menu-toggle').on('click', function() {
-	if ($('#wrapper').hasClass('toggled')) {
-		return $('#menu-toggle').html('<i class="fa fa-times fa-2x" aria-hidden="true"></i>');
-	}
-	else {
-		return $('#menu-toggle').html('<i class="fa fa-bars fa-2x" aria-hidden="true"></i>');
-	}
-});
+// $('#menu-toggle').on('click', function() {
+// 	if ($('#wrapper').hasClass('toggled')) {
+// 		return $('#menu-toggle').html('<i class="fa fa-times fa-2x" aria-hidden="true"></i>');
+// 	}
+// 	else {
+// 		return $('#menu-toggle').html('<i class="fa fa-bars fa-2x" aria-hidden="true"></i>');
+// 	}
+// });
 
-$(function() {
-	$('#myWork').bind('click',function(event){
-		var $anchor = $(this);
+// $(function() {
+// 	$('#myWork').bind('click',function(event){
+// 		var $anchor = $(this);
 
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
-		event.preventDefault();
-	});
-});
+// 		$('html, body').stop().animate({
+// 			scrollTop: $($anchor.attr('href')).offset().top
+// 		}, 1500,'easeInOutExpo');
+// 		/*
+// 		if you don't want to use the easing effects:
+// 		$('html, body').stop().animate({
+// 			scrollTop: $($anchor.attr('href')).offset().top
+// 		}, 1000);
+// 		*/
+// 		event.preventDefault();
+// 	});
+// });
 
-function1 = () => {
+allLineThrough = () => {
 	document.getElementById('all').className += 'linethrough';
 	document.getElementById('some').setAttribute("style", "display: initial;")	
 }
-function2 = () => {
+someLineThrough = () => {
 	document.getElementById('some').classList.add('linethrough');
 	document.getElementById('one').setAttribute("style", "display: initial;")
 }
-function3 = () => {
+oneLineThrough = () => {
 	document.getElementById('one').classList.add('linethrough');
 	document.getElementById('your').classList.add('linethrough');
 	document.getElementById('ok').setAttribute("style", "display: initial;")
 }
 
-var functions = [function1, function2, function3];
+var functions = [allLineThrough, someLineThrough, oneLineThrough];
 var i = 0;
 
 callsFunctions = () => {
 	functions[i++]();
 	if (i < functions.length) 
-	setTimeout(callsFunctions, 2000);	
+	setTimeout(callsFunctions, 3000);	
 } 
 
-setTimeout(callsFunctions, 1000);
+setTimeout(callsFunctions, 2500);
+
+
+var mainText = document.querySelector(".main_text");
+var aboutSection = document.querySelector(".about_section");
+var showAbout = document.querySelector(".display_about");
+var showMain = document.querySelector(".display_main");
+
+displayAboutText = () => {
+	mainText.classList.add('hidden')
+	aboutSection.classList.remove('hidden')
+}
+
+displayMainText = () => {
+	aboutSection.classList.add('hidden')
+	mainText.classList.remove('hidden')
+}
+
+showAbout.addEventListener('click', displayAboutText)
+showMain.addEventListener('click', displayMainText)
