5a5f17b9eae0c8df469cbaf0dd52e208f95b9762
Randy Galeano <randyagaleano@gmail.com>
2019-06-19 22:35:53 -0400

    cleanup and styling changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-// $('#menu-toggle').click(function(click) {
-//     click.preventDefault();
-//     $('#wrapper').toggleClass('toggled');
-// });
-
-// $('#menu-toggle').on('click', function() {
-// 	if ($('#wrapper').hasClass('toggled')) {
-// 		return $('#menu-toggle').html('<i class="fa fa-times fa-2x" aria-hidden="true"></i>');
-// 	}
-// 	else {
-// 		return $('#menu-toggle').html('<i class="fa fa-bars fa-2x" aria-hidden="true"></i>');
-// 	}
-// });
-
-// $(function() {
-// 	$('#myWork').bind('click',function(event){
-// 		var $anchor = $(this);
-
-// 		$('html, body').stop().animate({
-// 			scrollTop: $($anchor.attr('href')).offset().top
-// 		}, 1500,'easeInOutExpo');
-// 		/*
-// 		if you don't want to use the easing effects:
-// 		$('html, body').stop().animate({
-// 			scrollTop: $($anchor.attr('href')).offset().top
-// 		}, 1000);
-// 		*/
-// 		event.preventDefault();
-// 	});
-// });
-
 allLineThrough = () => {
 	document.getElementById('all').className += 'linethrough';
 	document.getElementById('some').setAttribute("style", "display: initial;")	
